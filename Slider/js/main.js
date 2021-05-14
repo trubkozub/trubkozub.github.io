ee10b075102bbb81f4caffd3f911a545f0d67487
trubkozub <ser.trubkozub@gmail.com>
2021-05-14 09:19:37 +0300

    ok

---
+++ Slider/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.serf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/Arrow-left.svg" alt=""></a>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/Arrow-right.svg" alt=""></a>',
+
+    });
+});
